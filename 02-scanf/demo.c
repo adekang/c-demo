1a41230374c2be204f24fe7ba5cca66212aba6c8
adekang <adekang@163.com>
2022-06-19 22:25:09 +0800

    sanf printf 使用

--- 02-scanf/demo.c
+++ 02-scanf/demo.c
@@ -1,6 +1,11 @@
 // 鏍煎紡鍖栬緭鍏?
 #include <stdio.h>
 
+// %c：一个单个的字符。
+// %f：小数（包含float类型和double类型）。
+// %d：十进制整数
+// %s：字符串。
+
 int main()
 {
   int a;
@@ -8,17 +13,16 @@ int main()
   char c;
   char str[10];
 
-  scanf("%d", &a);
-  scanf("%f", &b);
+  scanf("%d%f", &a, &b);
   // getchar
   getchar();
   scanf("%c", &c);
   scanf("%s", str);
 
-  printf("int var = %d\n", a);
-  printf("float var = %f\n", b);
-  printf("char var = %c\n", c);
-  printf("string var = %s\n", str);
+  printf("int var a = %d\n", a);
+  printf("float var b = %f\n", b);
+  printf("char var c = %c\n", c);
+  printf("string var str = %s\n", str);
 
   return 0;
 }
