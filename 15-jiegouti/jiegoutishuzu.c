cb18fbd2b99cc4f2dc9a0c6070525c08e619f7e0
adekang <adekang@163.com>
2022-07-04 21:45:23 +0800

    结构体指针

--- 15-jiegouti/jiegoutishuzu.c
+++ 15-jiegouti/jiegoutishuzu.c
@@ -5,30 +5,41 @@ struct student
   int num; //结构体成员
   char name[20];
   char sex;
-  float score;
 };
 
-// 结构体
+// 结构体指针
 int main()
 {
   struct student s =
       {
           1001,
           "乐乐",
-          'm',
-          20};
-  struct student arr[3];
-  int i;
+          'm'};
+  struct student *p;
+  p = &s;
 
-  for (i = 0; i < 3; i++)
-  {
-    scanf("%d%s%c%f", &arr[i].num, arr[i].name, &arr[i].sex, &arr[i].score);
-  }
-  for (i = 0; i < 3; i++)
-  {
-    printf("%d,%s,%c,%5.2f\n", arr[i].num, arr[i].name, arr[i].sex, arr[i].score);
-  }
-  printf("%d,%s,%c,%5.2f", s.num, s.name, s.sex, s.score);
+  printf("%d,%s,%c\n", (*p).num, (*p).name, (*p).sex);
+  // 指针成员选择
+  printf("%d,%s,%c\n", p->num, p->name, p->sex);
+
+  // 结构体指针偏移
+
+  struct student arr[3] = {1001,
+                           "乐乐",
+                           'm', 1005,
+                           "乐乐",
+                           'm', 1008,
+                           "乐乐",
+                           'm'};
+
+  p = arr;
+
+  printf("___________________\n");
+  int num;
+  num = p->num++;
+  printf("num = %d,p->num = %d\n", num, p->num);
+  num = p++->num;
+  printf("num = %d,p->num = %d\n", num, p->num);
 
   return 0;
 }
