09b0e574edbf3ea520e022c0d0362add4dd008b7
adekang <adekang@163.com>
2022-07-13 22:17:11 +0800

    尾插法

--- 02-xianxingbiao/shuanglianbiao.cpp
+++ 02-xianxingbiao/shuanglianbiao.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int ElemType; // 顺序表中元素的类型
 typedef struct DNode
 {
   int data;
@@ -34,6 +35,34 @@ DLinkList DListHeadInsert(DLinkList &DL)
   return DL;
 }
 
+/* 尾插法 */
+DLinkList DListTailInsert(DLinkList &DL)
+{
+  int x;
+  DL = (DLinkList)malloc(sizeof(DNode)); // 创建带头结点的链表
+  DLinkList s, r = DL;                   // r代表尾指针，指向最后一个节点
+  DL->prior = NULL;
+  DL->next = NULL;
+  scanf("%d", &x);
+  // 3 4 5 6 9999
+  while (x != 9999)
+  {
+    s = (DLinkList)malloc(sizeof(DNode));
+    s->data = x;
+    r->next = s;
+    s->prior = r;
+    r = s; // r指向新的表尾结点
+    scanf("%d", &x);
+  }
+  r->next = NULL; // 将最后一个节点的next指针指向NULL
+  return DL;
+}
+
+/* 新节点插入第几个位置 头插法 */
+DLinkList DListFrontInsert(DLinkList &DL, int i, ElemType e)
+{
+}
+
 // 链表遍历打印
 void PrintList(DLinkList L)
 {
@@ -53,6 +82,14 @@ int main()
   DLinkList search;
 
   /* 头插法 */
-  DListHeadInsert(DL);
+  // DListHeadInsert(DL);
+  // PrintList(DL);
+
+  /* 尾插法 */
+  DListTailInsert(DL);
+  PrintList(DL);
+
+  /* 新节点插入第几个位置 头插法 */
+  DListFrontInsert(DL, 2, 44);
   PrintList(DL);
 }
