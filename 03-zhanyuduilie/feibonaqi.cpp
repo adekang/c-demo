a64350d150808dd40bec1d47a62e738133caf1dd
adekang <adekang@163.com>
2022-07-21 21:47:21 +0800

    斐波那契数列

--- 03-zhanyuduilie/feibonaqi.cpp
+++ 03-zhanyuduilie/feibonaqi.cpp
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
+int Fib(int num)
+{
+  if (num == 0)
+  {
+    return 0;
+  }
+  else if (num == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return Fib(num - 1) + Fib(num - 2);
+  }
+}
+
 // 斐波那契数列
 int main()
 {
+
+  int num;
+  while (scanf("%d", &num) != EOF)
+  {
+    printf("总和：%d\n", num, Fib(num));
+  }
+
   return 0;
 }
